910bb7085a8dbded0b92c05932edfe44d5c08f99
ian333 <sebastianvaz123@hotmail.com>
2022-05-19 17:35:55 -0500

    First Run
    Signed-off-by: ian333 <sebastianvaz123@hotmail.com>

--- database.py
+++ database.py
@@ -4,8 +4,10 @@ import sqlalchemy.orm as _orm
 
 DATABASE_URL = "sqlite:///./database.db"
 
-engine=_sql.create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
+engine = _sql.create_engine(DATABASE_URL, connect_args={
+                            "check_same_thread": False})
 
-SessionLocal = _orm.sessionmaker(autocommit=False,autoflush=False,bind=engine)
+SessionLocal = _orm.sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
 
-Base= _declarative.declarative_base()
+Base = _declarative.declarative_base()
--- models.py
+++ models.py
@@ -5,27 +5,29 @@ import passlib.hash as _hash
 
 import database as _database
 
+
 class User(_database.Base):
     __tablename__ = "users"
-    id=_sql.Column(_sql.Integer,primary_key=True,index=True )
-    email=_sql.Column(_sql.String,unique=True,index=True)
-    hashed_password=_sql.Column(_sql.String)
-    
-    leads=_orm.relationship("Lead",back_populates="owner")
+    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    email = _sql.Column(_sql.String, unique=True, index=True)
+    hashed_password = _sql.Column(_sql.String)
 
-    def verify_password(self,password:str):
-        _hash.bcrypt.verify(password,self.hashed_password)
+    leads = _orm.relationship("Lead", back_populates="owner")
+
+    def verify_password(self, password: str):
+        _hash.bcrypt.verify(password, self.hashed_password)
+
 
 class Lead(_database.Base):
-    __tablename__="leads"
-    id=_sql.Column(_sql.Integer,primary_key=True,index=True)
-    owner_id=_sql.Column(_sql.Integer,_sql.ForeignKey("users.id"))
-    first_name=_sql.Column(_sql.String,index=True)
-    last_name=_sql.Column(_sql.String,index=True)
-    email=_sql.Column(_sql.String,index=True)
-    company=_sql.Column(_sql.String,index=True,default="")
-    note=_sql.Column(_sql.String,default="")
-    date_created=_sql.Column(_sql.DateTime,default=dt.datetime.utcnow)
-    date_last_updated=_sql.Column(_sql.DateTime,default=dt.datetime.utcnow)
+    __tablename__ = "leads"
+    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    owner_id = _sql.Column(_sql.Integer, _sql.ForeignKey("users.id"))
+    first_name = _sql.Column(_sql.String, index=True)
+    last_name = _sql.Column(_sql.String, index=True)
+    email = _sql.Column(_sql.String, index=True)
+    company = _sql.Column(_sql.String, index=True, default="")
+    note = _sql.Column(_sql.String, default="")
+    date_created = _sql.Column(_sql.DateTime, default=dt.datetime.utcnow)
+    date_last_updated = _sql.Column(_sql.DateTime, default=dt.datetime.utcnow)
 
-    owner=_orm.relationship("User",back_populates="leads")
+    owner = _orm.relationship("User", back_populates="leads")
